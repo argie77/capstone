25d317a2ebf17966f405f1ae57cc43b167a2e681
argie <argeline0729@yahoo.com>
2022-06-10 15:07:53 -0400

    order page is showing up

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -5,7 +5,24 @@ import styles from "../../styles/Order.module.css";
 const Order = () => {
   return (
     <div>
-        hello
+      <div className={styles.container}>
+        <div className={styles.left}> </div>
+        <div className={styles.right}>
+        <div className={styles.wrapper}>
+        <h2 className={styles.title}>Cart Total</h2>
+        <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal:</b>$29.98
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount:</b>$0.00
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total:</b>$29.98
+          </div>
+          <button disabled className={styles.button}>Paid</button>
+         </div>    
+         </div>
+      </div>
     </div>
   )
 }
