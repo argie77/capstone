cfa7bfa929853a29dee47a23f13160c81f180164
argie <argeline0729@yahoo.com>
2022-06-09 23:45:56 -0400

    useState is now working

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,9 +1,11 @@
 import styles from "../../styles/Product.module.css";
 import Image from "next/image";
+import { useState } from "react";
+
 
 const Product = () => {
   //useState is use to get the 1st price and the 2nd price
-  //const [size, setSize] = useState(0);
+  const [size, setSize] = useState(0);
 const pasta = {
     id:1,
     img: "/img/photo-1616299908398-9af1134ad522.avif",
@@ -21,9 +23,9 @@ const pasta = {
       </div>
       <div className={styles.right}>
         <h1 className={styles.title}>{pasta.name}</h1>
-        {/* <span className={styles.price}>${pasta.price[size]}</span>
-        // <p className={styles.desc}>{pasta.desc}</p>
-        <h3 className={styles.choose}>Choose the size</h3> */}
+        <span className={styles.price}>${pasta.price[size]}</span>
+         <p className={styles.desc}>{pasta.desc}</p>
+        <h3 className={styles.choose}>Choose the size</h3>
       </div>
         </div>
   )
